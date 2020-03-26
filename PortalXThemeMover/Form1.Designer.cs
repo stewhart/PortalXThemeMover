fa01ce3f56d3608ceed168236f9b6abc695a09e0
stewhart <60789259+stewhart@users.noreply.github.com>
2020-03-26 10:20:02 +0000

    Add files via upload

--- PortalXThemeMover/Form1.Designer.cs
+++ PortalXThemeMover/Form1.Designer.cs
@@ -28,12 +28,258 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCopyTheme = new System.Windows.Forms.Button();
+            this.comboBoxListOfThemes = new System.Windows.Forms.ComboBox();
+            this.buttonGetThemes = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxPass2 = new System.Windows.Forms.TextBox();
+            this.textBoxRestAdd2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LabelUser2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUser2 = new System.Windows.Forms.TextBox();
+            this.textBoxPass1 = new System.Windows.Forms.TextBox();
+            this.textBoxUser1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxRestAdd1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCopyTheme);
+            this.panel1.Controls.Add(this.comboBoxListOfThemes);
+            this.panel1.Controls.Add(this.buttonGetThemes);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBoxPass2);
+            this.panel1.Controls.Add(this.textBoxRestAdd2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.LabelUser2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxUser2);
+            this.panel1.Controls.Add(this.textBoxPass1);
+            this.panel1.Controls.Add(this.textBoxUser1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBoxRestAdd1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(442, 316);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonCopyTheme
+            // 
+            this.buttonCopyTheme.Location = new System.Drawing.Point(12, 290);
+            this.buttonCopyTheme.Name = "buttonCopyTheme";
+            this.buttonCopyTheme.Size = new System.Drawing.Size(418, 23);
+            this.buttonCopyTheme.TabIndex = 16;
+            this.buttonCopyTheme.Text = "Copy to Destination";
+            this.buttonCopyTheme.UseVisualStyleBackColor = true;
+            this.buttonCopyTheme.Click += new System.EventHandler(this.buttonCopyTheme_Click);
+            // 
+            // comboBoxListOfThemes
+            // 
+            this.comboBoxListOfThemes.FormattingEnabled = true;
+            this.comboBoxListOfThemes.Location = new System.Drawing.Point(9, 150);
+            this.comboBoxListOfThemes.Name = "comboBoxListOfThemes";
+            this.comboBoxListOfThemes.Size = new System.Drawing.Size(416, 21);
+            this.comboBoxListOfThemes.TabIndex = 15;
+            // 
+            // buttonGetThemes
+            // 
+            this.buttonGetThemes.Location = new System.Drawing.Point(8, 120);
+            this.buttonGetThemes.Name = "buttonGetThemes";
+            this.buttonGetThemes.Size = new System.Drawing.Size(418, 23);
+            this.buttonGetThemes.TabIndex = 14;
+            this.buttonGetThemes.Text = "Get Themes from Source";
+            this.buttonGetThemes.UseVisualStyleBackColor = true;
+            this.buttonGetThemes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Destination REST Address";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxPass2
+            // 
+            this.textBoxPass2.Location = new System.Drawing.Point(143, 264);
+            this.textBoxPass2.Name = "textBoxPass2";
+            this.textBoxPass2.Size = new System.Drawing.Size(284, 20);
+            this.textBoxPass2.TabIndex = 11;
+            this.textBoxPass2.UseSystemPasswordChar = true;
+            // 
+            // textBoxRestAdd2
+            // 
+            this.textBoxRestAdd2.Location = new System.Drawing.Point(143, 210);
+            this.textBoxRestAdd2.Name = "textBoxRestAdd2";
+            this.textBoxRestAdd2.Size = new System.Drawing.Size(284, 20);
+            this.textBoxRestAdd2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(289, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Theme Destination Details";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(244, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Theme Source Details";
+            // 
+            // LabelUser2
+            // 
+            this.LabelUser2.AutoSize = true;
+            this.LabelUser2.Location = new System.Drawing.Point(8, 240);
+            this.LabelUser2.Name = "LabelUser2";
+            this.LabelUser2.Size = new System.Drawing.Size(57, 13);
+            this.LabelUser2.TabIndex = 7;
+            this.LabelUser2.Text = "UserName";
+            this.LabelUser2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "UserName";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source REST Address";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxUser2
+            // 
+            this.textBoxUser2.Location = new System.Drawing.Point(143, 237);
+            this.textBoxUser2.Name = "textBoxUser2";
+            this.textBoxUser2.Size = new System.Drawing.Size(284, 20);
+            this.textBoxUser2.TabIndex = 3;
+            // 
+            // textBoxPass1
+            // 
+            this.textBoxPass1.Location = new System.Drawing.Point(142, 94);
+            this.textBoxPass1.Name = "textBoxPass1";
+            this.textBoxPass1.Size = new System.Drawing.Size(284, 20);
+            this.textBoxPass1.TabIndex = 2;
+            this.textBoxPass1.UseSystemPasswordChar = true;
+            // 
+            // textBoxUser1
+            // 
+            this.textBoxUser1.Location = new System.Drawing.Point(142, 67);
+            this.textBoxUser1.Name = "textBoxUser1";
+            this.textBoxUser1.Size = new System.Drawing.Size(284, 20);
+            this.textBoxUser1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(158, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBoxRestAdd1
+            // 
+            this.textBoxRestAdd1.Location = new System.Drawing.Point(142, 40);
+            this.textBoxRestAdd1.Name = "textBoxRestAdd1";
+            this.textBoxRestAdd1.Size = new System.Drawing.Size(284, 20);
+            this.textBoxRestAdd1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(468, 353);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "PortalXThemeMoverMagic";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LabelUser2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUser2;
+        private System.Windows.Forms.TextBox textBoxPass1;
+        private System.Windows.Forms.TextBox textBoxUser1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxRestAdd1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxPass2;
+        private System.Windows.Forms.TextBox textBoxRestAdd2;
+        private System.Windows.Forms.Button buttonCopyTheme;
+        private System.Windows.Forms.ComboBox comboBoxListOfThemes;
+        private System.Windows.Forms.Button buttonGetThemes;
     }
 }
 
--- PortalXThemeMover/Form1.cs
+++ PortalXThemeMover/Form1.cs
@@ -7,14 +7,131 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Globalization;
+using System.Web.Script.Serialization;
+using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Linq;
+using StarRezApi;
 
 namespace PortalXThemeMover
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+           
+
+
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBoxListOfThemes.Items.Clear();
+           QuerySourceTemplates();
+        }
+
+        public void QuerySourceTemplates()
+        {
+            StarRezApiClient sourceClient = new StarRezApiClient(textBoxRestAdd1.Text, textBoxUser1.Text, textBoxPass1.Text);
+            var SourceTemplates = sourceClient.Query("SELECT * FROM PortalTheme");
+            String test = SourceTemplates.ElementAt(2).Description.ToString();
+            
+            globalclass.ThemeType = UpdateDropDown(SourceTemplates);
+            Console.WriteLine("test");
+            
+        }
+
+        public Dictionary<string, string> UpdateDropDown(dynamic[] ThemesToShow)
+        {
+            dynamic results = ThemesToShow;
+            int themeCounter = 0;
+            comboBoxListOfThemes.DisplayMember = "Text";
+            comboBoxListOfThemes.ValueMember = "Value";
+            Dictionary<string, string> ThemeTypeCalc = new Dictionary<string, string>();
+            foreach (ApiObject theme in results)
+            {
+                Console.WriteLine(results[themeCounter].Description.ToString());
+                comboBoxListOfThemes.Items.Add(new { Text = results[themeCounter].Description.ToString(), Value = results[themeCounter].PortalThemeID.ToString() });
+                ThemeTypeCalc.Add(results[themeCounter].PortalThemeID.ToString(), results[themeCounter].ThemeLayout.ToString());
+                themeCounter++;
+            }
+
+            return ThemeTypeCalc;
+
+        }
+
+        private void buttonCopyTheme_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine(globalclass.ThemeType.First().ToString());
+            int TempID = Int32.Parse(((comboBoxListOfThemes.SelectedItem as dynamic).Value));
+            Console.WriteLine(TempID.ToString());
+            QueryGetTemplateSetting(TempID);
+
+        }
+
+        public void QueryGetTemplateSetting(int ThemeID)
+        {
+            string getThemeSettings = @"SELECT * FROM PortalSetting WHERE TableName = ""PortalTheme"" AND TableID = " + ThemeID;
+            StarRezApiClient sourceClient = new StarRezApiClient(textBoxRestAdd1.Text, textBoxUser1.Text, textBoxPass1.Text);
+            var SourceSettings = sourceClient.Query(getThemeSettings);
+           // String test = SourceSettings.ElementAt(2).Description.ToString();
+            Console.WriteLine("test");
+            int newThemeID = createTheme(ThemeID);
+            addSettings(SourceSettings, newThemeID);
+
+        }
+
+        public void addSettings(dynamic settingList, int targetThemeID)
+        {
+            int settingCounter = 0;
+            StarRezApiClient settingClient = new StarRezApiClient(textBoxRestAdd2.Text, textBoxUser2.Text, textBoxPass2.Text);
+            var newSetting = settingClient.CreateDefault("PortalSetting");
+            foreach (ApiObject theme in settingList)
+            {
+                newSetting.TableID = targetThemeID;
+                newSetting.TableName = "PortalTheme";
+                newSetting.Description = settingList[settingCounter].Description;
+                newSetting.Value = settingList[settingCounter].Value;
+
+
+                Console.WriteLine(settingList[settingCounter].Description.ToString());
+                //  comboBoxListOfThemes.Items.Add(new { Text = results[themeCounter].Description.ToString(), Value = results[themeCounter].PortalThemeID.ToString() });
+                settingClient.Create(newSetting);
+                settingCounter++;
+            }
+            MessageBox.Show("Settings created... hopefully");
+        }
+
+        public int createTheme(int ThemeID)
+        {
+            StarRezApiClient createThemeClient = new StarRezApiClient(textBoxRestAdd2.Text, textBoxUser2.Text, textBoxPass2.Text);
+            var newTheme = createThemeClient.CreateDefault("PortalTheme");
+            newTheme.Description = ThemeID.ToString() + "-" + ((comboBoxListOfThemes.SelectedItem as dynamic).Text);
+            string result;
+            globalclass.ThemeType.TryGetValue(ThemeID.ToString(), out result);
+            newTheme.ThemeLayout = result;
+            int newThemeID = createThemeClient.Create(newTheme);
+            Console.WriteLine(newThemeID.ToString());
+            return newThemeID;
+
+        }
+        static class globalclass
+        {
+            public static Dictionary<string, string> ThemeType = new Dictionary<string, string>();
+        }
+
     }
+
+
+
 }
